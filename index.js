38bc82e7c21fbe23a3e2a20168b2a2340136463a
jose-guerrerog <joseguerrerog880@gmail.com>
2024-06-04 06:31:05 +1000

    adding env variables

--- index.js
+++ index.js
@@ -1,68 +1,81 @@
-const express = require('express');
-const Filter = require('bad-words')
+const express = require("express");
+const Filter = require("bad-words");
 const app = express();
-const http = require('http');
+const http = require("http");
 const { Server } = require("socket.io");
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-const { generateMessage } = require('./utils/messages')
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+const { generateMessage } = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
+  cors: { origin: process.env.REACT_APP_URL, methods: ["GET", "POST"] },
 });
 
 io.on("connection", (socket) => {
   console.log(`a user connected ${socket.id}`);
-  
-  socket.on('join', (options, callback) => {
-    const { error, user } = addUser({ id: socket.id, ...options })
+
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
 
     if (error) {
-      return callback(error)
+      return callback(error);
     }
 
-    socket.join(user.room)
+    socket.join(user.room);
 
-    io.to(user.room).emit('message', generateMessage('Admin', 'Welcome!'))
-    //socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
-    io.to(user.room).emit('roomData', {
-        room: user.room,
-        users: getUsersInRoom(user.room)
-    })
+    io.to(user.room).emit("message", generateMessage("Admin", "Welcome!"));
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-    callback()
-  })
+    callback();
+  });
 
-
-  socket.on('sendMessage', (message) => {
-    const user = getUser(socket.id)
-    const filter = new Filter()
+  socket.on("sendMessage", (message) => {
+    const user = getUser(socket.id);
+    const filter = new Filter();
 
     if (filter.isProfane(message)) {
-        return callback('Profanity is not allowed!')
+      return callback("Profanity is not allowed!");
     }
 
-    io.to(user.room).emit('message', generateMessage(user.username, message))
-  })
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+  });
 
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id)
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
 
     if (user) {
-      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
-      io.to(user.room).emit('roomData', {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("Admin", `${user.username} has left!`)
+      );
+      io.to(user.room).emit("roomData", {
         room: user.room,
-        users: getUsersInRoom(user.room)
-      })
+        users: getUsersInRoom(user.room),
+      });
     }
-  })
+  });
 
   socket.on("send_message", (data) => {
     io.emit("receive_message", data);
   });
 });
 
-server.listen(4000, () => { console.log("listening on *:4000"); });
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
